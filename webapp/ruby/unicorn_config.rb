f9cc460135871e1fc353a326e66b0a53c085b882
admin <test@example.com>
2016-04-28 18:53:01 +0900

    unix domain socket

--- webapp/ruby/unicorn_config.rb
+++ webapp/ruby/unicorn_config.rb
@@ -1,6 +1,7 @@
 worker_processes 2
 preload_app true
-listen "127.0.0.1:8080"
+# listen "127.0.0.1:8080"
+listen "/tmp/unicorn.sock"
 
 stderr_path "/tmp/unicorn.log"
 stdout_path "/tmp/unicorn.log"
