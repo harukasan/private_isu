b202e25d2b0cf07eb934ab403eaa71a3783de99e
moyashipan <moyashipan@gmail.com>
2016-04-28 17:50:55 +0900

    localhost -> 127.0.0.1

--- webapp/ruby/app.rb
+++ webapp/ruby/app.rb
@@ -34,7 +34,7 @@ module Isuconp
       end
 
       def cache
-        @cache ||= Dalli::Client.new 'localhost:11211'
+        @cache ||= Dalli::Client.new '127.0.0.1:11211'
       end
 
       def db
